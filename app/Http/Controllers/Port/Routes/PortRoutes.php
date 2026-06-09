6941a3237a6723492850453da6bf0326e6a3bfd7
Anand-1315 <anand.s@ardhas.com>
2026-06-09 14:35:40 +0530

    Port Security Access
    Vehicle Status file and functionality adede

--- app/Http/Controllers/Port/Routes/PortRoutes.php
+++ app/Http/Controllers/Port/Routes/PortRoutes.php
@@ -48,6 +48,8 @@ Route::middleware(['web', 'is_login'])->group(function () {
     Route::get('portsecurity/security_access/export/excel', 'Port\PortSecurityController@ExportExcel');
     Route::get('portsecurity/security_access/export/pdf', 'Port\PortSecurityController@ExportPdf');
     Route::get('portsecurity/security_access/pdf/{id}', 'Port\PortSecurityController@ExportViewPdf');
+    Route::get('portsecurity/security_access/vehicle_status/{id}', 'Port\PortSecurityController@VehicleStatus');
+    Route::post('portsecurity/security_access/vehicle_status/submit', 'Port\PortSecurityController@VehicleStatusAdd');
 
 
 });
